b352582f0a0957f8b63e06b2e223616d84abc875
Alplox <42575576+Alplox@users.noreply.github.com>
2019-06-02 16:30:21 -0400

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -11,7 +11,7 @@ wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/DLM.part4.rar
 wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/DLM.part3.rar
 wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/DLM.part2.rar
 wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/DLM.part1.rar
-unrar x DLM.part01.rar
+unrar x DLM.part1.rar
 #
 cp -R /home/ubuntu/DLM (carpeta)/DLM.rar  /var/www/html/ && cd /var/www/html && unzip DLM.rar
 cp /var/www/html/downloads/iptv_panel_pro.zip /tmp/ && chmod a+x /tmp/iptv_panel_pro.zip
