abe93f2da2afb678c5998671f273763d3431e2b1
Alplox <42575576+Alplox@users.noreply.github.com>
2019-06-02 17:13:11 -0400

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -14,7 +14,9 @@ wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/DLM.part1.rar
 unrar x DLM.part1.rar
 #
 cp -R /home/ubuntu/DLMc/DLM.rar  /var/www/html/ && cd /var/www/html && unrar x DLM.rar
-cp /var/www/html/DLM/downloads/iptv_panel_pro.zip /tmp/ && chmod a+x /tmp/iptv_panel_pro.zip && unzip /var/www/html//DLM/downloads/install_iptv_pro.zip && cp /var/www/html/DLM/downloads/install_iptv_pro.php /root/ && cd /root && chmod a+x /root/install_iptv_pro.php
+cp /var/www/html/DLM/downloads/iptv_panel_pro.zip /tmp/ && chmod a+x /tmp/iptv_panel_pro.zip && unzip /var/www/html/DLM/downloads/install_iptv_pro.zip
+cd /var/www/html/DLM/downloads/
+cp install_iptv_pro.php /root/ && cd /root/ && chmod a+x /root/install_iptv_pro.php
 #
 #
 echo "####################################################################################"
