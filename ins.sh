de4b398a75e5f583c1fb84f7988ffa4719de6077
Alplox <42575576+Alplox@users.noreply.github.com>
2019-06-02 15:54:10 -0400

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -2,20 +2,31 @@
 #
 apt-get update && apt-get upgrade -y
 apt-get install apache2 -y
-apt-get install lsb-release nscd curl php5 php5-mysql php5-cli php5-curl unzip -y && apt-get install php5-mcrypt -y &&  php5enmod mcrypt
+apt-get install lsb-release nscd curl php5 php5-mysql php5-cli php5-curl unzip unrar -y && apt-get install php5-mcrypt -y &&  php5enmod mcrypt
 service apache2 restart
-wget http://downloads.sourceforge.net/project/iptv-md/xtream/www_dir.tar.gz -O /tmp/www_dir.tar.gz
+wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/www_dir.part08.rar
+wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/www_dir.part07.rar
+wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/www_dir.part06.rar
+wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/www_dir.part05.rar
+wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/www_dir.part04.rar
+wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/www_dir.part03.rar
+wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/www_dir.part02.rar
+wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/www_dir.part01.rar -O /tmp/
 #
 if [ -d /var/www/html ];
 then
     echo "/var/www/html/ exists."
-	tar -zxvf /tmp/www_dir.tar.gz -C /var/www/html/
+	cp -r downloads/ /var/www/html/
+	cp -r licences/ /var/www/html/
+	cp -r modules/ /var/www/html/
 else
     echo " "
 	if [ -d /root/www/ ];
 	then
     echo "/var/www/ exist"
-    	tar -zxvf /tmp/www_dir.tar.gz -C /var/www/
+    	cp -r downloads/ /var/www/
+	cp -r licences/ /var/www/
+	cp -r modules/ /var/www/
 else
     echo "No Webserver installed?"
 
