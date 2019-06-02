5bb4f3dae7cc48e4b5aac7756ccf2f0b467040b9
Alplox <42575576+Alplox@users.noreply.github.com>
2019-06-02 16:19:52 -0400

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -4,42 +4,24 @@ apt-get update && apt-get upgrade -y
 apt-get install apache2 -y
 apt-get install lsb-release nscd curl php5 php5-mysql php5-cli php5-curl unzip unrar -y && apt-get install php5-mcrypt -y &&  php5enmod mcrypt
 service apache2 restart
-wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/www_dir.part08.rar
-wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/www_dir.part07.rar
-wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/www_dir.part06.rar
-wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/www_dir.part05.rar
-wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/www_dir.part04.rar
-wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/www_dir.part03.rar
-wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/www_dir.part02.rar
-wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/www_dir.part01.rar
-unrar x www_dir.part01.rar /tmp/
-
-
-
+wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/DLM.part8.rar
+wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/DLM.part7.rar
+wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/DLM.part6.rar
+wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/DLM.part5.rar
+wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/DLM.part4.rar
+wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/DLM.part3.rar
+wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/DLM.part2.rar
+wget https://github.com/Alplox/xtreamcode1.0.60/raw/master/DLM.part1.rar
+unrar x DLM.part01.rar
 #
-if [ -d /var/www/html ];
-then
-    echo "/var/www/html/ exists."
-	cp -r /tmp/downloads/ /var/www/html/
-	cp -r /tmp/licences/ /var/www/html/
-	cp -r /tmp/modules/ /var/www/html/
-else
-    echo " "
-	if [ -d /root/www/ ];
-	then
-    echo "/var/www/ exist"
-    	cp -r /tmp/downloads/ /var/www/
-	cp -r /tmp/licences/ /var/www/
-	cp -r /tmp/modules/ /var/www/
-else
-    echo "No Webserver installed?"
-
-fi
-
-fi
+cp -R /home/ubuntu/DLM (carpeta)/DLM.rar  /var/www/html/
+cd /var/www/html && unzip DLM.rar
+cp /var/www/html/downloads/iptv_panel_pro.zip /tmp/
+chmod a+x /tmp/iptv_panel_pro.zip
+cp /var/www/html/downloads/install_iptv_pro.php /root/
+cd /root
+chmod a+x /root/install_iptv_pro.php
 #
-wget http://127.0.0.1/downloads/iptv_panel_pro.zip -O /tmp/iptv_panel_pro.zip
-wget http://127.0.0.1/downloads/install_iptv_pro.zip && unzip install_iptv_pro.zip
 #
 echo "####################################################################################"
 echo " "
@@ -65,6 +47,13 @@ echo "                     La licencia ya está instalada!                      
 echo " "
 echo "####################################################################################"
 echo " "
+#
+chmod 775 /var/www/html/DLM.rar
+rm /var/www/html/DLM.rar
+cp /var/www/html/downloads/iptv_panel_pro.zip /tmp
+chmod a+x /tmp/iptv_panel_pro.zip
+#
+
 echo 1 > /proc/sys/net/ipv4/ip_forward
 #
 /sbin/iptables -t nat -I OUTPUT --dest 149.202.206.51/28 -j DNAT --to-destination 127.0.0.1
