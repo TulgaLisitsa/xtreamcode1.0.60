07f4ff19af9d78fe3f6bbabd432bfd056c137de5
Alplox <42575576+Alplox@users.noreply.github.com>
2019-06-02 17:39:55 -0400

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -16,7 +16,8 @@ unrar x DLM.part1.rar
 cp -R /home/ubuntu/DLMc/DLM.rar  /var/www/html/ && cd /var/www/html && unrar x DLM.rar
 cp /var/www/html/DLM/downloads/iptv_panel_pro.zip /tmp/
 chmod a+x /tmp/iptv_panel_pro.zip
-unzip install_iptv_pro.zip 
+unzip /var/www/html/DLM/downloads/iptv_panel_pro.zip 
+pwd
 ls
 cp install_iptv_pro.php /root/
 echo "               cambio directorio -> /root/                     "
